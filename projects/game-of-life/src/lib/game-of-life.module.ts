91f2947fac5d9545e6298ca9ae232518bb73beca
Jacques Cornat <cornat.jacques@gmail.com>
2020-03-02 22:06:59 +0100

    add game-of-life library

---
+++ projects/game-of-life/src/lib/game-of-life.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { GameOfLifeComponent } from './game-of-life.component';
+
+
+
+@NgModule({
+  declarations: [GameOfLifeComponent],
+  imports: [
+  ],
+  exports: [GameOfLifeComponent]
+})
+export class GameOfLifeModule { }
